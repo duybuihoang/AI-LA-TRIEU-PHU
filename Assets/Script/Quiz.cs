20333a7ccbede97ef33326e177b7a3e77002cea3
duybuihoang <91789217+duybuihoang@users.noreply.github.com>
2024-05-19 20:27:36 +0700

    feat: add picking answers

--- Assets/Script/Quiz.cs
+++ Assets/Script/Quiz.cs
@@ -1,8 +1,9 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.ComponentModel;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
@@ -14,14 +15,18 @@ public class Quiz : MonoBehaviour
     [SerializeField] Sprite correctAnswerSprite;
 
 
-
-
     void Start()
+    {
+        getNextQuestion();
+        // displayQuestion();
+        //setButtonState(true);
+    }
+    private void displayQuestion()
     {
         questionText.text = question.getQuestionText();
         setQuestion();
-    }
 
+    }
     private void setQuestion()
     {
         for (int i = 0; i < answerButtons.Length; i++)
@@ -31,8 +36,48 @@ public class Quiz : MonoBehaviour
         }
     }
 
-    void Update()
+    public void onAnswerSelected(int index)
     {
-        
+        Image imageButton;
+        if (index == question.getCorrectAnswerIndex())
+        {
+            questionText.text = "Chính xác!";
+            imageButton = answerButtons[index].GetComponent<Image>();
+            imageButton.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correcAnswerIndex = question.getCorrectAnswerIndex();
+            string correcAnswer = question.getAnswers(correcAnswerIndex);
+            questionText.text = "Sai, Câu trả lời chính xác là\n" + correcAnswer;
+            imageButton = answerButtons[correcAnswerIndex].GetComponent<Image>();
+            imageButton.sprite = correctAnswerSprite;
+        }
+        setButtonState(false);
+    }
+
+    void setButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+            
+        }
+    }
+
+    private void getNextQuestion()
+    {
+        setButtonState(true);
+        resetButton();
+        displayQuestion();
+    }
+    void resetButton()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Image imageButton = answerButtons[i].GetComponent<Image>();
+            imageButton.sprite = defaulAnswerSprite;
+        }
     }
 }
